ecbce8011a200ce026c9dbd46a6e4e4c5f14dda5
Abreal <127664084+AbrshS@users.noreply.github.com>
2023-05-03 14:13:22 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,35 @@
+# Rock Paper Scissor game
+# Name: Abraham Shiferaw
+# ID: 0704/12
+
 import random
 
-def get_user_move():
-    moves = ["rock", "paper", "scissors"]
-    while True:
-        move = input("Enter your move (rock, paper, scissors): ")
-        if move in moves:
-            return move
-        else:
-            print("Invalid move. Please try again.")
-
-def get_computer_move():
-    return random.choice(["rock", "paper", "scissors"])
-
-def compare_moves(user_move, computer_move):
-    if user_move == computer_move:
-        return "tie"
-    elif (user_move == "rock" and computer_move == "scissors") or (user_move == "paper" and computer_move == "rock") or (user_move == "scissors" and computer_move == "paper"):
-        return "user"
-    else:
-        return "computer"
-
-def print_statistics(user_score, computer_score, draw_count):
-    print("Player score:", user_score)
-    print("Computer score:", computer_score)
-    print("Draw count:", draw_count)
-
-# Initialize the player's and computer's scores
-user_score = 0
+moves = ["rock", "paper", "scissors"]
+player_score = 0
 computer_score = 0
 draw_count = 0
-
-# Start the game loop
 while True:
-
-    # Get the player's move
-    user_move = get_user_move()
-
-    # Get the computer's move
-    computer_move = get_computer_move()
-
-    # Compare the moves
-    winner = compare_moves(user_move, computer_move)
-    if winner == "tie":
+    player_move = input("Enter your move (rock, paper, scissors): ")
+    computer_move = random.choice(moves)
+    print('Computer move: ', computer_move)
+    if player_move == computer_move:
         print("It's a tie!")
         draw_count += 1
-    elif winner == "user":
-        print("You win! {} beats {}".format(user_move, computer_move))
-        user_score += 1
+    elif player_move == "rock" and computer_move == "scissors":
+        print("You win! Rock smashes scissors.")
+        player_score += 1
+    elif player_move == "paper" and computer_move == "rock":
+        print("You win! Paper covers rock.")
+        player_score += 1
+    elif player_move == "scissors" and computer_move == "paper":
+        print("You win! Scissors cuts paper.")
+        player_score += 1
     else:
-        print("Computer wins! {} beats {}".format(computer_move, user_move))
+        print("Computer wins!")
         computer_score += 1
-
-    # Check if the player wants to play again
     play_again = input("Would you like to play again? (y/n): ")
     if play_again == "n":
         break
-
-# Print the final statistics
-print_statistics(user_score, computer_score, draw_count)
+print("Player score:", player_score)
+print("Computer score:", computer_score)
+print("Draw count:", draw_count)
